bf0594e55901ac7020dc432c066bd7ffff2d685d
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2023-05-16 23:07:35 +0530

    table_design_1
    Giving table styles and animations

--- src/components/AttendanceTable.js
+++ src/components/AttendanceTable.js
@@ -1,10 +1,8 @@
-import { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase';
-import './AttendanceTable.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { motion } from "framer-motion";
+import "./AttendanceTable.css";
 
 export function AttendanceTable() {
   const [attendanceData, setAttendanceData] = useState([]);
@@ -13,12 +11,12 @@ export function AttendanceTable() {
     async function fetchAttendanceData() {
       try {
         const subjectCode = "21CS41";
-        const attendanceRef = collection(db, 'ISE', 'attendance', subjectCode);
+        const attendanceRef = collection(db, "ISE", "attendance", subjectCode);
         const snapshot = await getDocs(attendanceRef);
-        const attendanceDocs = snapshot.docs.map(doc => doc.data());
+        const attendanceDocs = snapshot.docs.map((doc) => doc.data());
         setAttendanceData(attendanceDocs);
       } catch (error) {
-        console.error('Error fetching attendance data from Firestore', error);
+        console.error("Error fetching attendance data from Firestore", error);
       }
     }
 
@@ -27,51 +25,102 @@ export function AttendanceTable() {
 
   const getClassCount = () => {
     return attendanceData.length;
-  }
+  };
 
   const getAttendanceCount = (sUSN) => {
     return attendanceData.reduce((total, data) => {
-      const student = data.attendance.find(student => student.sUSN === sUSN);
+      const student = data.attendance.find((student) => student.sUSN === sUSN);
       return total + (student && student.Present ? 1 : 0);
     }, 0);
-  }
+  };
 
   const getAttendancePercentage = (attendanceCount, classCount) => {
-    return classCount > 0 ? ((attendanceCount / classCount) * 100).toFixed(2) : 'N/A';
-  }
+    return classCount > 0
+      ? ((attendanceCount / classCount) * 100).toFixed(2)
+      : "N/A";
+  };
 
   return (
-    <div className="table-responsive" 
-    style={{paddingLeft: "5svw", paddingRight: "5svw"}}>
-    <table style={{padding: '30px'}}>
-      <thead>
-        <tr>
-          <th>USN</th>
-          <th>Name</th>
-          <th>Classes Held</th>
-          <th>Classes Attended</th>
-          <th>Attendance Percentage</th>
-          {attendanceData.map(data => (
-            <th key={data.date.toMillis()}>{data.date.toDate().toLocaleDateString()}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {attendanceData[0]?.attendance.map(student => (
-          <tr key={student.sUSN}>
-            <td>{student.sUSN}</td>
-            <td>{student.sName}</td>
-            <td>{getClassCount()}</td>
-            <td>{getAttendanceCount(student.sUSN)}</td>
-            <td>{getAttendancePercentage(getAttendanceCount(student.sUSN), getClassCount())}%</td>
-            {attendanceData.map(data => {
-              const attendanceRecord = data.attendance.find(record => record.sUSN === student.sUSN);
-              return <td key={`${data.date.toMillis()}-${student.sUSN}`} class={attendanceRecord?.Present ? 'attendance-presentt' : 'attendance-absentt'}>{attendanceRecord?.Present ? 'P' : 'A'}</td>;
-            })}
+    <div
+      className="table-responsive"
+      style={{ paddingLeft: "5svw", paddingRight: "5svw" }}
+    >
+      <table>
+        <thead>
+          <tr>
+            <th style={{backgroundColor: "#43286D"}}>USN</th>
+            <th style={{backgroundColor: "#DE2527"}}>Name</th>
+            <th style={{backgroundColor: "#43286D"}}>Classes Held</th>
+            <th style={{backgroundColor: "#DE2527"}}>Classes Attended</th>
+            <th style={{backgroundColor: "#43286D"}}>Attendance Percentage</th>
+            {attendanceData.map((data) => (
+              <th style={{backgroundColor: "black"}} key={data.date.toMillis()}>
+                {data.date.toDate().toLocaleDateString()}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {attendanceData[0]?.attendance.map((student) => (
+            <tr key={student.sUSN}
+            >
+              <motion.td
+              style={{backgroundColor: "#43286D"}}
+              initial={{ scale:1 }}
+              transition={{type:"spring"}}
+              whileHover={{scale:1.5}}
+              >
+              <a 
+              style={{textDecoration:"none", color:"inherit"}} 
+              href="https://www.w3schools.com/html/html_links.asp"> {/* make function to send teacher to that student data page on clicking the USN */}
+                {student.sUSN}
+              </a>
+              </motion.td>
+              <motion.td
+              style={{backgroundColor: "#DE2527"}}
+              initial={{ scale:1 }}
+              transition={{type:"spring"}}
+              whileHover={{scale:1.5}}
+              >{student.sName}</motion.td>
+              <motion.td
+              style={{backgroundColor: "#43286D"}}
+              >{getClassCount()}</motion.td>
+              <motion.td
+              style={{backgroundColor: "#DE2527"}}
+              >{getAttendanceCount(student.sUSN)}</motion.td>
+              <motion.td
+              style={{backgroundColor: "#43286D"}}
+                initial={{ scale:1 }}
+                transition={{type:"spring"}}
+                whileHover={{scale:1.5}}
+              >
+                {getAttendancePercentage(
+                  getAttendanceCount(student.sUSN),
+                  getClassCount()
+                )}
+                %
+              </motion.td>
+              {attendanceData.map((data) => {
+                const attendanceRecord = data.attendance.find(
+                  (record) => record.sUSN === student.sUSN
+                );
+                return (
+                  <motion.td
+                    key={`${data.date.toMillis()}-${student.sUSN}`}
+                    className={
+                      attendanceRecord.Present
+                        ? "attendance-presentt"
+                        : "attendance-absentt"
+                    }
+                  >
+                    {attendanceRecord?.Present ? "P" : "A"}
+                  </motion.td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
