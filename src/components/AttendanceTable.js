103391d86c3fe4417b3ee01d96357221bcbeaf8a
Darshan Gowda <96766940+darshan-sr@users.noreply.github.com>
2023-05-17 21:44:50 +0530

    Update AttendanceTable.js

--- src/components/AttendanceTable.js
+++ src/components/AttendanceTable.js
@@ -11,34 +11,33 @@ export function AttendanceTable() {
     async function fetchAttendanceData() {
       try {
         const subjectCode = "21CS41";
-        const attendanceRef = collection(db, "ISE", "attendance", subjectCode);
+        const attendanceRef = collection(db, 'ISE', 'attendance', subjectCode);
         const snapshot = await getDocs(attendanceRef);
-        const attendanceDocs = snapshot.docs.map((doc) => doc.data());
+        const attendanceDocs = snapshot.docs.map(doc => doc.data());
         setAttendanceData(attendanceDocs);
       } catch (error) {
-        console.error("Error fetching attendance data from Firestore", error);
+        console.error('Error fetching attendance data from Firestore', error);
       }
     }
 
     fetchAttendanceData();
-  }, []);
+  }, []); 
 
   const getClassCount = () => {
     return attendanceData.length;
-  };
+  }
 
   const getAttendanceCount = (sUSN) => {
     return attendanceData.reduce((total, data) => {
-      const student = data.attendance.find((student) => student.sUSN === sUSN);
+      const student = data.attendance.find(student => student.sUSN === sUSN);
       return total + (student && student.Present ? 1 : 0);
     }, 0);
-  };
+  }
 
   const getAttendancePercentage = (attendanceCount, classCount) => {
-    return classCount > 0
-      ? ((attendanceCount / classCount) * 100).toFixed(2)
-      : "N/A";
-  };
+    return classCount > 0 ? ((attendanceCount / classCount) * 100).toFixed(2) : 'N/A';
+  }
+
 
   return (
     <div
