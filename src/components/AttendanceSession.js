b52b9b44cddfe495ff223265bd23d9d160a9263e
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2023-05-17 22:29:35 +0530

    h

--- src/components/AttendanceSession.js
+++ src/components/AttendanceSession.js
@@ -1,18 +1,15 @@
-
-
-import { useState, useEffect } from 'react';
-import StudentCard from './StudentCard';
-import data from './data';
-import { collection, addDoc,setDoc,doc, Timestamp } from 'firebase/firestore'; // adddoc not used
-import { db } from './firebase';
-import "./App.css"
-import Select from 'react-select'
-import "./style.css"
+import { useState, useEffect } from "react";
+import StudentCard from "./StudentCard";
+import data from "./data";
+import { collection, addDoc, setDoc, doc, Timestamp } from "firebase/firestore"; // adddoc not used
+import { db } from "./firebase";
+import "./App.css";
+import Select from "react-select";
+import "./style.css";
 import success from "../Images/success.png";
-import FacultySidebar from './FacultySidebar'; // not used
+import FacultySidebar from "./FacultySidebar"; // not used
 import { useUserAuth } from "./context/UserAuthContext";
-import { AttendanceTable } from './AttendanceTable';
-
+import { AttendanceTable } from "./AttendanceTable";
 
 export default function AttendanceSession() {
   const [attendance, setAttendance] = useState(data);
@@ -23,7 +20,6 @@ export default function AttendanceSession() {
   const [totalSteps, setTotalSteps] = useState(3); // not used
   const [selectedSubject, setSelectedSubject] = useState(null);
   const [selectedSession, setSelectedSession] = useState(null);
-  
 
   const [currentStep, setCurrentStep] = useState(1);
 
@@ -41,35 +37,36 @@ export default function AttendanceSession() {
       try {
         const subjectCode = selectedSubject.value;
         const Time = new Date().toISOString();
-        
 
-        const subjectRef = collection(db,  'attendance', subjectCode, Time ); //subjectRef not used
+        const subjectRef = collection(db, "attendance", subjectCode, Time); //subjectRef not used
         const newAttendanceDoc = {
           date: Timestamp.now(),
-          attendance: attendance.map(student => ({
+          attendance: attendance.map((student) => ({
             sName: student.sName,
             sUSN: student.sUSN,
             Present: student.Present,
-            
           })),
           presentCount: presentCount,
           absentCount: absentCount,
           updatedBy: user.displayName,
         };
-        await setDoc(doc(db, 'ISE', 'attendance', subjectCode, Time ), newAttendanceDoc);
+        await setDoc(
+          doc(db, "ISE", "attendance", subjectCode, Time),
+          newAttendanceDoc
+        );
       } catch (error) {
-        console.error('Error writing to Firestore', error);
+        console.error("Error writing to Firestore", error);
       }
     }
-  
+
     if (attendance.length > 0 && presentCount > 0) {
       updateFirestore();
     }
   }, [attendance, presentCount, absentCount, selectedSubject]); //React Hook useEffect has a missing dependency: 'user.displayName'. Either include it or remove the dependency array
 
   function toggleAttendance(usn) {
-    setAttendance(prevAttendance => {
-      return prevAttendance.map(student => {
+    setAttendance((prevAttendance) => {
+      return prevAttendance.map((student) => {
         if (student.sUSN === usn) {
           return { ...student, Present: !student.Present };
         } else {
@@ -83,7 +80,7 @@ export default function AttendanceSession() {
     let presentCount = 0;
     let absentCount = 0;
 
-    attendance.forEach(student => {
+    attendance.forEach((student) => {
       if (student.Present) {
         presentCount++;
       } else {
@@ -97,7 +94,7 @@ export default function AttendanceSession() {
     window.scrollTo(0, 0);
   }
 
-  const studentCards = attendance.map(student => (
+  const studentCards = attendance.map((student) => (
     <StudentCard
       key={student.sUSN}
       img={student.Image}
@@ -111,23 +108,25 @@ export default function AttendanceSession() {
   ));
 
   const subjectOptions = [
-    { value: '21CS41', label: '21CS41 (Mathematical Foundations for Computing)' },
-    { value: '21CS42', label: '21CS42 (Design and Analysis of Algorithms)' },
-    { value: '21CS43', label: '21CS43 (Microcontroller and Embedded System)' },
-    { value: '21CS44', label: '21CS44 (Operating System)' },
-    { value: '21BE45', label: '21BE45 (Biology for Engineers)' },
+    {
+      value: "21CS41",
+      label: "21CS41 (Mathematical Foundations for Computing)",
+    },
+    { value: "21CS42", label: "21CS42 (Design and Analysis of Algorithms)" },
+    { value: "21CS43", label: "21CS43 (Microcontroller and Embedded System)" },
+    { value: "21CS44", label: "21CS44 (Operating System)" },
+    { value: "21BE45", label: "21BE45 (Biology for Engineers)" },
   ];
-  
+
   const sessionOptions = [
-    { value: '9-10am', label: '9:00am - 10:00am' },
-    { value: '3214', label: '10:00am -11:00am' },
-    { value: '21CS423543', label: '11:00am - 12:00pm' },
-    { value: '21C35S44', label: '12:20pm - 1:20pm' },
-    { value: '21BE545', label: '2:00pm - 3:00pm' },
-    { value: '21BE545', label: '3:00pm - 4:00pm' },
+    { value: "9-10am", label: "9:00am - 10:00am" },
+    { value: "3214", label: "10:00am -11:00am" },
+    { value: "21CS423543", label: "11:00am - 12:00pm" },
+    { value: "21C35S44", label: "12:20pm - 1:20pm" },
+    { value: "21BE545", label: "2:00pm - 3:00pm" },
+    { value: "21BE545", label: "3:00pm - 4:00pm" },
   ];
-  
-  
+
   const handleSubjectChange = (selectedOption) => {
     setSelectedSubject(selectedOption);
   };
@@ -137,54 +136,111 @@ export default function AttendanceSession() {
   };
 
   const stepOne = (
-   <div>
-       <h4 style={{paddingBottom: '10px', textAlign: 'center'}}>Create new Session</h4>
+    <div>
+      <h4 style={{ paddingBottom: "10px", textAlign: "center" }}>
+        Create new Session
+      </h4>
 
-       <div style={{display: 'flex', justifyContent: 'center'}}>
-       <div style={{ width: '360px',zIndex: '0' }}>
-       <p htmlFor="subjectSelect" style={{paddingBottom: '1px',marginBottom: '3px'}}>Choose Subject:</p>
-       <Select id="subjectSelect" value={selectedSubject} onChange={handleSubjectChange} options={subjectOptions}  /></div>
-       </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ width: "360px", zIndex: "0" }}>
+          <p
+            htmlFor="subjectSelect"
+            style={{ paddingBottom: "1px", marginBottom: "3px" }}
+          >
+            Choose Subject:
+          </p>
+          <Select
+            id="subjectSelect"
+            value={selectedSubject}
+            onChange={handleSubjectChange}
+            options={subjectOptions}
+          />
+        </div>
+      </div>
 
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ width: "360px" }}>
+          <p
+            htmlFor="sessionSelect"
+            style={{
+              paddingBottom: "1px",
+              marginBottom: "3px",
+              paddingTop: "20px",
+            }}
+          >
+            Choose Session Time:
+          </p>
+          <Select
+            id="sessionSelect"
+            value={selectedSession}
+            onChange={handleSessionChange}
+            options={sessionOptions}
+          />
+        </div>
+      </div>
 
-       <div style={{display: 'flex', justifyContent: 'center'}}>
-       <div style={{ width: '360px' }}>
-       <p htmlFor="sessionSelect" style={{paddingBottom: '1px',marginBottom: '3px',paddingTop: '20px'}}>Choose Session Time:</p>
-       <Select id="sessionSelect" value={selectedSession} onChange={handleSessionChange} options={sessionOptions} /></div>
-       </div>
-
-
-       <button className="submitAttendance"  onClick={() => {handleNext(); setProgress(1);}}>Next</button>
-
+      <button
+        className="submitAttendance"
+        onClick={() => {
+          handleNext();
+          setProgress(1);
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 
-
   const stepTwo = (
-    <div className="mainContainer" style={{ overflow: 'hidden'}}>
-      <h4 style={{paddingBottom: '10px', textAlign: 'center'}}>Take Attendance</h4>
+    <div className="mainContainer" style={{ overflow: "hidden" }}>
+      <h4 style={{ paddingBottom: "10px", textAlign: "center" }}>
+        Take Attendance
+      </h4>
       {studentCards}
       {attendance.length > 0 && presentCount === 0 && (
         <div className="buttonContainer">
-        {progress > 0 && (
-            <button className="multibutton" onClick={() => {setProgress(progress - 1); handlePrev();}}>
+          {progress > 0 && (
+            <button
+              className="multibutton"
+              onClick={() => {
+                setProgress(progress - 1);
+                handlePrev();
+              }}
+            >
               Previous
             </button>
           )}
-          <button className="multibutton" onClick={() => {submitAttendance(); handleNext(); setProgress(2);}}>
+          <button
+            className="multibutton"
+            onClick={() => {
+              submitAttendance();
+              handleNext();
+              setProgress(2);
+            }}
+          >
             Submit
           </button>
         </div>
       )}
     </div>
   );
-  
-const stepThree = (
-  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',}}>
-   <img src={success} alt="Success" style={{ maxHeight: '150px'}} />
-    <h4 style={{ paddingBottom: '10px', textAlign: 'center',  }}> Attendance Recorded </h4>
 
-    {presentCount > 0 && (
+  const stepThree = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img src={success} alt="Success" style={{ maxHeight: "150px" }} />
+      <h4 style={{ paddingBottom: "10px", textAlign: "center" }}>
+        {" "}
+        Attendance Recorded{" "}
+      </h4>
+
+      {presentCount > 0 && (
         <p>
           {presentCount}/{attendance.length} Present
         </p>
@@ -194,39 +250,48 @@ const stepThree = (
           {absentCount}/{attendance.length} Absent
         </p>
       )}
-    <button className="submitAttendance" onClick={() => {setProgress(0); handlePrev(); handlePrev()}}>Return Home</button>
-  </div>
-);
+      <button
+        className="submitAttendance"
+        onClick={() => {
+          setProgress(0);
+          handlePrev();
+          handlePrev();
+        }}
+      >
+        Return Home
+      </button>
+    </div>
+  );
 
-return (
-  <div className="App" style={{paddingTop: '95px'}}>
-        <AttendanceTable/>
-        <div className="progresscontainer">  
-      <div className="steps">
-        {[1, 2, 3].map((step) => (
-          <span
-            key={step}
-            className={`circle ${currentStep >= step ? "active" : ""}`}
-          >
-            {step}
-          </span>
-        ))}
-        <div className="progress-bar">
-          <span
-            className="indicator"
-            style={{
-              width: `${((currentStep - 1) / 2) * 100}%`,
-            }}
-          ></span>
+  return (
+    <div className="App" style={{ paddingTop: "95px" }}>
+      <AttendanceTable />
+      <div className="progresscontainer">
+        <div className="steps">
+          {[1, 2, 3].map((step) => (
+            <span
+              key={step}
+              className={`circle ${currentStep >= step ? "active" : ""}`}
+            >
+              {step}
+            </span>
+          ))}
+          <div className="progress-bar">
+            <span
+              className="indicator"
+              style={{
+                width: `${((currentStep - 1) / 2) * 100}%`,
+              }}
+            ></span>
+          </div>
         </div>
       </div>
-      </div>
-    <header className="App-header">
-    </header>
-    <main className="containerr">
-      {progress === 0 && stepOne}
-      {progress === 1 && stepTwo}
-      {progress === 2 && stepThree}
-    </main>
-  </div>
-)};
+      <header className="App-header"></header>
+      <main className="containerr">
+        {progress === 0 && stepOne}
+        {progress === 1 && stepTwo}
+        {progress === 2 && stepThree}
+      </main>
+    </div>
+  );
+}
--- src/components/AttendanceTable.js
+++ src/components/AttendanceTable.js
@@ -13,120 +13,120 @@ export function AttendanceTable() {
     async function fetchAttendanceData() {
       try {
         const subjectCode = "21CS41";
-        const attendanceRef = collection(db, 'ISE', 'attendance', subjectCode);
+        const attendanceRef = collection(db, "ISE", "attendance", subjectCode);
         const snapshot = await getDocs(attendanceRef);
-        const attendanceDocs = snapshot.docs.map(doc => doc.data());
+        const attendanceDocs = snapshot.docs.map((doc) => doc.data());
         setAttendanceData(attendanceDocs);
       } catch (error) {
-        console.error('Error fetching attendance data from Firestore', error);
+        console.error("Error fetching attendance data from Firestore", error);
       }
     }
 
     fetchAttendanceData();
-  }, []); 
+  }, []);
 
   const getClassCount = () => {
     return attendanceData.length;
-  }
+  };
 
   const getAttendanceCount = (sUSN) => {
     return attendanceData.reduce((total, data) => {
-      const student = data.attendance.find(student => student.sUSN === sUSN);
+      const student = data.attendance.find((student) => student.sUSN === sUSN);
       return total + (student && student.Present ? 1 : 0);
     }, 0);
-  }
+  };
 
   const getAttendancePercentage = (attendanceCount, classCount) => {
-    return classCount > 0 ? ((attendanceCount / classCount) * 100).toFixed(2) : 'N/A';
-  }
-
+    return classCount > 0
+      ? ((attendanceCount / classCount) * 100).toFixed(2)
+      : "N/A";
+  };
 
   return (
-    <div
-      className="table-responsive"
-      style={{ paddingLeft: "5svw", paddingRight: "5svw" }}
-    >
-      <table>
-        <thead>
-          <tr>
-            <th style={bgcol2}>USN</th>
-            <th style={bgcol1}>Name</th>
-            <th style={bgcol2}>Classes Held</th>
-            <th style={bgcol1}>Classes Attended</th>
-            <th style={bgcol2}>Attendance Percentage</th>
-            {attendanceData.map((data) => (
-              <th
-                style={{ backgroundColor: "black" }}
-                key={data.date.toMillis()}
-              >
-                {data.date.toDate().toLocaleDateString()}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {attendanceData[0]?.attendance.map((student) => (
-            <tr key={student.sUSN}>
-              <motion.td
-                style={bgcol2}
-                initial={{ scale: 1 }}
-                transition={{ type: "spring" }}
-                whileHover={{ scale: 1.5 }}
-              >
-                <a
-                  style={{ textDecoration: "none", color: "inherit" }}
-                  href="https://www.w3schools.com/html/html_links.asp"
+    <div className="Main_BOx">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th style={bgcol2}>USN</th>
+              <th style={bgcol1}>Name</th>
+              <th style={bgcol2}>Classes Held</th>
+              <th style={bgcol1}>Classes Attended</th>
+              <th style={bgcol2}>Attendance Percentage</th>
+              {attendanceData.map((data) => (
+                <th
+                  style={{ backgroundColor: "black" }}
+                  key={data.date.toMillis()}
                 >
-                  {" "}
-                  {/* make function to send teacher to that student data page on clicking the USN */}
-                  {student.sUSN}
-                </a>
-              </motion.td>
-              <motion.td
-                style={bgcol1}
-                initial={{ scale: 1 }}
-                transition={{ type: "spring" }}
-                whileHover={{ scale: 1.5 }}
-              >
-                {student.sName}
-              </motion.td>
-              <motion.td style={bgcol2}>{getClassCount()}</motion.td>
-              <motion.td style={bgcol1}>
-                {getAttendanceCount(student.sUSN)}
-              </motion.td>
-              <motion.td
-                style={bgcol2}
-                initial={{ scale: 1 }}
-                transition={{ type: "spring" }}
-                whileHover={{ scale: 1.5 }}
-              >
-                {getAttendancePercentage(
-                  getAttendanceCount(student.sUSN),
-                  getClassCount()
-                )}
-                %
-              </motion.td>
-              {attendanceData.map((data) => {
-                const attendanceRecord = data.attendance.find(
-                  (record) => record.sUSN === student.sUSN
-                );
-                return (
-                  <motion.td
-                    key={`${data.date.toMillis()}-${student.sUSN}`}
-                    className={
-                      attendanceRecord.Present
-                        ? "attendance-presentt"
-                        : "attendance-absentt"
-                    }
-                  >
-                    {attendanceRecord?.Present ? "P" : "A"}
-                  </motion.td>
-                );
-              })}
+                  {data.date.toDate().toLocaleDateString()}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {attendanceData[0]?.attendance.map((student) => (
+              <tr key={student.sUSN}>
+                <motion.td
+                  style={bgcol2}
+                  initial={{ scale: 1 }}
+                  transition={{ type: "spring" }}
+                  whileHover={{ scale: 1.1 }}
+                >
+                  <a
+                    style={{ textDecoration: "none", color: "inherit" }}
+                    href="https://www.w3schools.com/html/html_links.asp"
+                  >
+                    {" "}
+                    {/* make function to send teacher to that student data page on clicking the USN */}
+                    {student.sUSN}
+                  </a>
+                </motion.td>
+                <motion.td
+                  style={bgcol1}
+                  initial={{ scale: 1 }}
+                  transition={{ type: "spring" }}
+                  whileHover={{ scale: 1.1 }}
+                >
+                  {student.sName}
+                </motion.td>
+                <motion.td style={bgcol2}>{getClassCount()}</motion.td>
+                <motion.td style={bgcol1}>
+                  {getAttendanceCount(student.sUSN)}
+                </motion.td>
+                <motion.td
+                  style={bgcol2}
+                  initial={{ scale: 1 }}
+                  transition={{ type: "spring" }}
+                  whileHover={{ scale: 1.1 }}
+                >
+                  {getAttendancePercentage(
+                    getAttendanceCount(student.sUSN),
+                    getClassCount()
+                  )}
+                  %
+                </motion.td>
+                {attendanceData.map((data) => {
+                  const attendanceRecord = data.attendance.find(
+                    (record) => record.sUSN === student.sUSN
+                  );
+                  return (
+                    <motion.td
+                      key={`${data.date.toMillis()}-${student.sUSN}`}
+                      className={
+                        attendanceRecord.Present
+                          ? "attendance-presentt"
+                          : "attendance-absentt"
+                      }
+                    >
+                      {attendanceRecord?.Present ? "P" : "A"}
+                    </motion.td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
