46a8739c1ed8ca83c36486acfe9cb12fdfd531a6
Dhyaan1 <65714425+Dhyaan1@users.noreply.github.com>
2023-06-04 03:30:07 +0530

    Cgpa wait section design

--- src/components/Student/CGPA/CgpaMarksDashboard.js
+++ src/components/Student/CGPA/CgpaMarksDashboard.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import StudentTopNavbar from '../MobileNav/StudentTopNavbar'
+import React from "react";
+import StudentTopNavbar from "../MobileNav/StudentTopNavbar";
+import "./markscalc.css";
+import { motion } from "framer-motion";
 
 const CgpaMarksDashboard = () => {
   return (
     <>
-     <StudentTopNavbar text={'Marks'}/>
+      <StudentTopNavbar text={"Marks"} />
 
-     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',}} className='text-center'>
-  <div>
-    Coming Soon
-  </div>
-</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+        className="text-center"
+      >
+        <div>Coming Soon</div>
+      </div>
+      <div className="rick"></div>
+      <motion.div drag class="inner-construction"></motion.div>
+      <motion.div
+        className="dance"
+        animate={{
+          scale: [1.4, 1, 1.4, 1, 1.4],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          rotate: [0, 0, 270, 270, 0],
+          x: [-100, 0, 100, 0, -100],
+          backgroundColor: [
+            "#9e1d65",
+            "#9e1d6400",
+            "#9e1d65",
+            "#9e1d6400",
+            "#9e1d65",
+          ],
+        }}
+        transition={{ duration: 2, repeat: Infinity }}
+      ></motion.div>
     </>
-  )
-}
+  );
+};
 
-export default CgpaMarksDashboard
+export default CgpaMarksDashboard;
